b5d963c91e2c3529bddb08b3f38068c704c3fd69
Myxy1 <myxy555@gmail.com>
2016-11-11 10:59:13 +0100

    2016. 11. 11.

---
+++ src/bevezetes/Bevezetes.java
@@ -0,0 +1,178 @@
+/*
+Készítette
+*/
+package bevezetes;
+
+
+public class Bevezetes {
+
+
+    public static void main(String[] args) {
+        /*
+        System.out.println("HORDA FOR EVER");
+        System.out.println("LEL");
+        System.out.println("szia!\n \t a nevem lel");
+        System.out.println("A \tkedvenc \t tantárgyam:\n\t\t\t A programozás");
+        *műveletek
+        * 
+        * 
+        System.out.print("5+3=");
+        System.out.println(5+3);
+        System.out.println("100-30=" + (100-30));
+        System.out.println("20*3=" + (20*3));
+        System.out.println("100/5=" + (100/5));
+        System.out.println("5/2=" + (5.0/2.0));
+        System.out.println("5 mod 3=" + (5%3));
+        System.out.println("-5 mod 3=" + (-5%3));
+        
+        
+        
+        //deklarálás:(változók)
+        
+        int a;
+        
+        //inicializálás (1-nek értéket adunk
+        
+        a = 1;
+        
+        System.out.println("Az 'a' változó értéke:");
+        System.out.println(a);
+        
+        int b = 2;
+        
+        System.out.print("A 'b' változó értéke:");
+        System.out.println(b);
+        
+        System.out.println("A 'b' változó értéke:" + b);
+        
+        System.out.println("a+b=" + (a) + "+" + (b) + "=" + (a+b) + "\tÖSSZEADÁS");
+        System.out.println("a-b=" + (a) + "-" + (b) + "=" + (a-b) + "\tKIVONÁS");
+        System.out.println("a*b=" + (a) + "*" + (b) + "=" + (a*b) + "\tSZORZÁS");
+        System.out.println("a/b=" + (a) + "/" + (b) + "=" + (a/b) + "\tOSZTÁS");
+        System.out.println("a%b=" + (a) + "%" + (b) + "=" + (a%b) + "\tMARADÉKOS OSZTÁS");
+        int c;
+        
+        c = 6*5;
+        
+        System.out.println("A 'c' változó értéke:" + (c));
+        
+        int d = c;
+        
+        System.out.println("A 'd' változó értéke:" + (d));
+        
+        c = 10;
+        System.out.println("c értéke:" + (c)  + "\nd értéke:" + (d));
+        System.out.println("d + c =" + (d+c));
+        
+        d = d+5;
+        System.out.println("új 'd':" + (d));
+        
+        
+        //deklarálunk egy törtet
+        
+        /* alma = alma+3;           =====   alma +=3 ; 
+        
+        double e = 3.12;
+        
+        double f = 5.24;
+        
+        double osszeg = e+f;
+        double kulonbseg =e-f;
+        double szorzat = e*f;
+        double hanyados = e/f;
+        
+        System.out.println("Összegük: " + osszeg);
+        System.out.println("Különbség: " + kulonbseg);
+        System.out.println("Szorzat: " + szorzat);
+        System.out.println("Hányadosuk: " + hanyados);
+        
+        
+        double c = 5;
+        
+        System.out.println("A változó értéke:" + c);
+        System.out.println(c*2);
+        
+        c += 3;
+        System.out.println("értéke:" + (c));
+        
+        int d=3;
+        System.out.println(d*10);
+        
+        double e;
+        e = c/d;
+        System.out.println(e);
+       
+        
+        double szam1 = -25.8;
+        double szam2 = 34.5;
+        
+        System.out.println("szam1 abszolúlt értéke: " + Math.abs(szam1));  //abszolúlt kerekítés
+        System.out.println("szam2 felfelé kerekítés: " + Math.ceil(szam2)); //felfelé
+        System.out.println("szam2 lefelé kerekítés: " + Math.floor(szam2)); //lefelé
+        System.out.println("egészre kerekítés: " + Math.round(szam2)); //kerekítés a rendszer szerint
+        
+        double szam3 = 2.5;
+        
+        System.out.println("egész részre: " + Math.floor(szam3));
+        System.out.println("tört része: " + (szam-Math.round(szam3));
+        
+        
+        
+        
+        
+        double szam3 = 45.6789;
+        
+        szam3 *=100;
+        
+        szam3 = Math.round(szam3);
+        
+        szam3 /=100;
+        
+        System.out.println(szam3);
+        
+        
+        int a = 3;
+        int b = 5;
+        
+        System.out.println("3 a négyzeten: " + (a*a)); //négyzetre emelés
+        System.out.println("3 a négyzeten: " + Math.pow(a, 2)); //négyzetre emelés
+        System.out.println("3 a negyediken: " + Math.pow(a,4));
+        System.out.println("5 a másodikon: " + Math.pow(b,2));
+        
+        //négyzetgyök vonás
+        
+        System.out.println("9 négyzetgyöke:" + Math.sqrt(9));
+        
+        
+        //minimum és maximum függvény
+        
+        int c=4;
+        int d=12;
+        
+        System.out.println("Legkisebb szám: " + Math.min(c, d));
+        System.out.println("Legnagyobb szám: " + Math.max(c, d));
+        
+   
+        
+        int d=5;
+        int e=8;
+        
+        System.out.println("legnagyobból kivonjuk a legkisebbet:" + (Math.max(d, e)-Math.min(e, d)));
+        
+        
+        int b=1;
+        int c=2;
+        int d=3;
+        int harmadik=Math.max(b, c);
+        
+        System.out.println("Legnagyob a három közül:" + Math.max(d,harmadik));
+        
+        */
+        
+        int d = 5;
+        int e = 9;
+        
+        System.out.println("Melyik a kisebb?" + Math.min(Math.pow(d,2) , Math.sqrt(e)));
+        
+    } 
+}
